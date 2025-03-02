de685206726022d3a8d226831635b8eb960862da
raveesha gimhan <146311393+R3aveesha@users.noreply.github.com>
2025-03-02 13:39:49 +0530

    update

---
+++ backend/App.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
