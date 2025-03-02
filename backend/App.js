ac8420d0a1e7d7e975ffc3ad4b0baa2a8e2b11e1
raveesha gimhan <146311393+R3aveesha@users.noreply.github.com>
2025-03-02 21:48:13 +0530

    update

--- backend/App.js
+++ backend/App.js
@@ -2,10 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 const cors = require("cors");
-
 const app = express();
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+dotenv.config();
+
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri)
+.then(() => console.log("MongoDB connected successfully"))
+.catch(err => console.error("MongoDB connection error:", err));
+
+
+
+app.listen(PORT, () => {
     console.log('Server is running on port 3000');
 });
 
