3cda9f3448b6b408dd229f9707dcae73d2f6a2b4
oshadha2k01 <oshadhanipun2k1@gmail.com>
2025-03-02 21:15:25 +0530

    Add Tailwind CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-3xl font-bold text-blue-500 bg-white p-4 rounded-lg shadow-lg">
+       Hello GalaxyX!!!
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
